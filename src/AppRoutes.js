6c542cd5a519f70d3a1ecb737a17b322065b386a
Gum <tokudam698@gmail.com>
2023-09-08 11:43:11 +0900

    route-edit

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -2,8 +2,10 @@ import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import { Top } from "./Top";
 import { SendMoney } from "./SendMoney";
+import { SelectMoney } from "./SelectMoney";
 
 export const router = createBrowserRouter([
   { path: "/", element: <Top /> },
   { path: "SendMoney", element: <SendMoney /> },
+  { path: "SelectMoney", element: <SelectMoney /> },
 ]);
