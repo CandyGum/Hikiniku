5c00e3d0cfaa464245d1af7c15258a5899c03c2e
SakuKWB <bb52123309@ms.nagasaki-u.ac.jp>
2023-09-08 10:43:33 +0900

    css追加

--- src/Top.js
+++ src/Top.js
@@ -1,26 +1,37 @@
 import React, {Component} from 'react';
 import bgImage from "./images/human1.png";
 import { useNavigate } from "react-router-dom";
+import './top.css';
 
 export const Top = () => {
 
   const navigate = useNavigate()
+
+//DBへのアクセスは後で
+  const zandaka = "10000";
+
   return (
     <html>
         <head>
         </head>
         <body>
         <div className="Step1">
+          <div className="container">
+            <figure class="image"><img src={bgImage} /></figure>
+            <div class="right">
+            <p class="title">ユーザー名:ひき肉食べたい</p>
+            <p class="text">口座番号:00000000<br/>
+            預金残高:{zandaka}円</p>
+            </div>
             
-            <img src={bgImage} />
-
-            <p>口座番号</p>
-            <p>ユーザー名</p>
-            <p>預金残高</p>
-            <p></p>
-            
+          </div> 
         </div>
-        <button onClick={() => navigate("SendMoney")}>送金画面へ</button>
+        <div class="button003">
+          <a hreh="#">
+          <button onClick={() => navigate("SendMoney")}>送金画面へ</button>
+          </a>
+        </div>
+        
         </body>
     </html>
   )
