714e052812306d898b57cdac9ac2517de14be649
ohoshi-sora <b2212710@photon.chitose.ac.jp>
2023-09-08 12:00:21 +0900

    selectmoney編集

--- src/SelectMoney.js
+++ src/SelectMoney.js
@@ -4,33 +4,6 @@ import bgImage2 from "./images/human2.png";
 import bgImage3 from "./images/human3.png";
 
 
-function TransactionScreen({ recipient }) {
-  const [amount, setAmount] = useState(0);
-
-  const handleAmountChange = (e) => {
-    setAmount(parseFloat(e.target.value));
-  };
-
-  const handleSendMoney = () => {
-    // 送金処理を実行するコードをここに追加
-  };
-
-  return (
-    <div>
-      <h1>送金金額を入力</h1>
-      <img src={recipient.image} alt={recipient.name} />
-      <p>{recipient.name}</p>
-      <p>送金上限金額: 1000円</p>
-      <input
-        type="number"
-        placeholder="金額を入力"
-        value={amount}
-        onChange={handleAmountChange}
-      />
-      <button onClick={handleSendMoney}>送金</button>
-    </div>
-  );
-}
 
 
 export const SelectMoney = () => {
@@ -46,19 +19,16 @@ export const SelectMoney = () => {
             <body>
             <div className="Step1">
               <div className="container">
-                <figure class="image"><img src={bgImage} /></figure>
-                <div class="right">
-                <p class="title">ユーザー名:ひき肉食べたい</p>
-                <p class="text">口座番号:00000000<br/>
-                預金残高:{zandaka}円</p>
-                </div>
-                
+                <figure class="image"><img src={bgImage1} /></figure>
+                {users.name}
               </div> 
             </div>
             <div class="button003">
-              <a hreh="#">
-              <button onClick={() => navigate("SendMoney")}>送金画面へ</button>
-              </a>
+                <input
+                    type="number"
+                    placeholder="金額を入力"
+                />
+                {/* <button onClick={handleSendMoney}>送金</button> */}
             </div>
             
             </body>
