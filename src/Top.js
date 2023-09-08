3df4e004b8920a6f6222a5558aea15c0767da2a7
SakuKWB <bb52123309@ms.nagasaki-u.ac.jp>
2023-09-08 09:25:00 +0900

    hyouzihennkou

--- src/Top.js
+++ src/Top.js
@@ -11,8 +11,14 @@ export const Top = () => {
         </head>
         <body>
         <div className="Step1">
-            <p>あ</p>
+            
             <img src={bgImage} />
+
+            <p>口座番号</p>
+            <p>ユーザー名</p>
+            <p>預金残高</p>
+            <p></p>
+            
         </div>
         <button onClick={() => navigate("SendMoney")}>送金画面へ</button>
         </body>
