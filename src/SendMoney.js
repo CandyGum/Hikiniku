e1b6102ff17f172cf9d4c6f3c805e3eadb8e8bb8
ohoshi-sora <b2212710@photon.chitose.ac.jp>
2023-09-08 09:50:01 +0900

    test

--- src/SendMoney.js
+++ src/SendMoney.js
@@ -1,14 +1,28 @@
 import React, {Component} from 'react';
 
 export const SendMoney = () => {
+
+  const users = [
+    {name:"サンプル太郎"},
+    {name:"サンプル次郎"},
+    {name:"サンプル三郎"}
+  ]
   return (
     <html>
         <head>
         </head>
         <body>
+        
         <div className="SendMoney">
-            <p>送金画面なう</p>
+            <p>送金相手を選択</p>
         </div>
+        
+
+        <ol>
+          {users.map((users, index) => (
+            <li key={index}>{users.name}</li>
+          ))}
+        </ol>
         </body>
     </html>
   )
